77b0fd0c401dc001f66852bcdaef9fba6189fb0b
Eric Dan <eric.dan8@gmail.com>
2017-07-13 19:44:53 -0400

    fix indentation

--- designday/designday.ino
+++ designday/designday.ino
@@ -4,20 +4,17 @@ int rmotor = 3;
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
-pinMode(lmotor,OUTPUT); 
-pinMode(rmotor,OUTPUT); 
-
-
+  pinMode(lmotor,OUTPUT); 
+  pinMode(rmotor,OUTPUT); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-analogWrite(lmotor,200);
-analogWrite(rmotor,200);
-
-delay(2000);
-
-analogWrite(lmotor,0);
-analogWrite(rmotor,0);
-
+  analogWrite(lmotor,200);
+  analogWrite(rmotor,200);
+  
+  delay(2000);
+  
+  analogWrite(lmotor,0);
+  analogWrite(rmotor,0);
 }
