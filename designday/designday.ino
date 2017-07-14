10e6a692e54de6fc32b713887b92e26977f270da
h94zhou <h94zhou@edu.uwaterloo.ca>
2017-07-14 13:55:09 -0400

    fix stuf

--- designday/designday.ino
+++ designday/designday.ino
@@ -1,3 +1,6 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial Genotronex(6,7);
 int lmotorSpeedPin = 11;
 int rmotorSpeedPin = 3;
 
@@ -16,7 +19,7 @@ int rpm = 0;
 int rotational_speed;
 unsigned long lastmillis = 0;
  
-
+int BluetoothData; // the data given from Computer
   
  // this code will be executed every time the interrupt 0 (pin2) gets low.
  void rpm_fan(){
@@ -33,10 +36,11 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(lmotorSpeedPin,OUTPUT); 
   pinMode(rmotorSpeedPin,OUTPUT); 
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
 //  desiredRPM = -1;
 //  attachInterrupt(digitalPinToInterrupt(2), rpm_fan, FALLING);
+
+  Genotronex.begin(9600);
+  Genotronex.println("Bluetooth On please press 1 or 0 blink LED ..");
 }
 
 void loop() {
@@ -65,32 +69,21 @@ void loop() {
   }
 
   current = analogRead(currentSensor);
-//  if (millis() - lastmillis > 1000) {
-//    
-//    detachInterrupt(0);//Disable interrupt when calculating
-//    rpm = half_revolutions * 60; // Convert frecuency to RPM, note: this works for one interruption per full rotation. For two interrups per full rotation use half_revolutions * 30.
-//    if (i > 9) {
-//      Serial.print("RPM=\t"); //print the word "RPM" and tab.
-//      Serial.print(rpm); // print the rpm value.
-//      Serial.print("\t Rotation speed=\t");
-//      Serial.print(rpm*2);
-//      Serial.print("PI rad/s");
-//      Serial.print("\t Hz=\t"); //print the word "Hz".
-//      Serial.print(half_revolutions); //print revolutions per second or Hz. And print new line or enter.
-//      Serial.print("\t Current=\t");
-//      Serial.println(current);
-//      i = 0;
-//    }
-//    half_revolutions = 0; // Restart the RPM counter
-//    lastmillis = millis(); // Uptade lasmillis
-//    attachInterrupt(digitalPinToInterrupt(2), rpm_fan, FALLING); //enable interrupt
-//    if (desiredRPM != -1 && desiredRPM != rpm) {
-//      if (rpm > desiredRPM) {
-//        speedInput -= 10;
-//      } else {
-//        speedInput += 10;
-//      }
-//      setMotors(speedInput);
-//    }
-//  }
+  // put your main code here, to run repeatedly:
+  if (Genotronex.available() > 0){
+    serialInput = Genotronex.readStringUntil(' ');
+    if (serialInput == "start") {
+      speedInput = Genotronex.parseInt();
+      setMotors(speedInput);
+      Genotronex.print("starting motor with speed ");
+      Genotronex.println(speedInput);
+      desiredRPM = -1;
+    }
+    
+    if (serialInput == "stop") {
+      setMotors(0);
+      desiredRPM = -1;
+    }
+
+  }
 }
